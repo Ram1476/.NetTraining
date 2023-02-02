199dea05ab58fc0e8c874c11390aee5a9bff2509
Ram1476 <ramilangovan.1299@gmail.com>
2023-02-02 10:08:27 +0530

    Add project files.

---
+++ Labexperiment2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labexperiment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* Event: Tennis Match\n\n* Venue: Star Sport Complex\n\n* Time: 4:00 p.m to 6:00 p.m");
+
+        }
+    }
+}
